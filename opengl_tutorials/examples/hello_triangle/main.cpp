9a952c8f869ca6646e9107cb8c7931b483380f5b
Igor Bogoslavskyi <igor.bogoslavskyi@gmail.com>
2020-03-21 23:44:38 +0100

    Add simple viewer

--- opengl_tutorials/examples/hello_triangle/main.cpp
+++ opengl_tutorials/examples/hello_triangle/main.cpp
@@ -2,6 +2,7 @@
 #include "opengl_tutorials/core/program.h"
 #include "opengl_tutorials/core/shader.h"
 #include "opengl_tutorials/core/vertex_array_buffer.h"
+#include "opengl_tutorials/ui/glfw/viewer.h"
 #include "opengl_tutorials/utils/eigen_utils.h"
 
 #include <GLFW/glfw3.h>
@@ -18,44 +19,10 @@ const gl_tutorials::eigen::vector<Eigen::Vector3f> vertices{
 };
 const std::vector<uint32_t> indices = {0, 1, 3, 1, 2, 3};  // Two triangles.
 
-void error_callback(int error, const char *description) {
-  std::cerr << "error[" << error << "]:" << description << std::endl;
-}
-
-void FramebufferSizeCallback(GLFWwindow *window, int width, int height) {
-  glViewport(0, 0, width, height);
-}
-
-void ProcessInput(GLFWwindow *window) {
-  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-    glfwSetWindowShouldClose(window, true);
-}
-
 int main(int argc, char const *argv[]) {
-  glfwSetErrorCallback(error_callback);
-  if (!glfwInit()) { exit(EXIT_FAILURE); }
-
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-  GLFWwindow *window =
-      glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
-
-  if (!window) {
-    glfwTerminate();
-    exit(EXIT_FAILURE);
-  }
-
-  glfwMakeContextCurrent(window);
-  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-    std::cerr << "Failed to initialize GLAD" << std::endl;
-    glfwTerminate();
-    exit(EXIT_FAILURE);
-  }
-
-  glViewport(0, 0, 800, 600);
-  glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
+  gl_tutorials::glfw::Viewer viewer{"OpenGlViewer"};
+  bool success = viewer.Initialize();
+  if (!success) { return EXIT_FAILURE; }
 
   const std::shared_ptr<gl_tutorials::Shader> vertex_shader{
       gl_tutorials::Shader::CreateFromFile(
@@ -69,8 +36,7 @@ int main(int argc, char const *argv[]) {
       {vertex_shader, fragment_shader});
   if (!program) {
     std::cerr << "Failed to link program" << std::endl;
-    glfwTerminate();
-    exit(EXIT_FAILURE);
+    return EXIT_FAILURE;
   }
 
   gl_tutorials::VertexArrayBuffer vertex_array_buffer{};
@@ -84,8 +50,8 @@ int main(int argc, char const *argv[]) {
       indices));
   vertex_array_buffer.EnableVertexAttributePointer(0);
 
-  while (!glfwWindowShouldClose(window)) {
-    ProcessInput(window);
+  while (!viewer.ShouldClose()) {
+    viewer.ProcessInput();
 
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
@@ -94,10 +60,7 @@ int main(int argc, char const *argv[]) {
 
     vertex_array_buffer.Draw(GL_TRIANGLES);
 
-    glfwPollEvents();
-    glfwSwapBuffers(window);
+    viewer.Spin();
   }
-  glfwDestroyWindow(window);
-  glfwTerminate();
-  exit(EXIT_SUCCESS);
+  return EXIT_SUCCESS;
 }
