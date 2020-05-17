c89147cbc650686b9c7670bbf4ff95e9bc8fbd91
igor.bogoslavskyi@uni-bonn.de <igor.bogoslavskyi@gmail.com>
2020-05-17 23:14:39 +0200

    Add initialization code and error handling

---
+++ gl/core/init.h
@@ -0,0 +1,40 @@
+// Copyright Igor Bogoslavskyi, year 2020.
+// In case of any problems with the code please contact me.
+// Email: <name>.<family_name>@gmail.com.
+
+#ifndef CODE_OPENGL_TUTORIALS_GL_CORE_INIT_H_
+#define CODE_OPENGL_TUTORIALS_GL_CORE_INIT_H_
+
+#include "glog/logging.h"
+#include "third_party/glad/glad.h"
+
+#include <functional>
+
+namespace gl {
+
+void GLAPIENTRY MessageCallback(GLenum source,
+                                GLenum type,
+                                GLuint id,
+                                GLenum severity,
+                                GLsizei length,
+                                const GLchar* message,
+                                const void* userParam) {
+  LOG(FATAL) << (type == GL_DEBUG_TYPE_ERROR ? "GL ERROR" : "") << " message: '"
+             << message << "'";
+}
+
+template <typename FunctionT>
+void InitializeGlContext(FunctionT intialization_function) {
+  if (!gladLoadGLLoader(
+          reinterpret_cast<GLADloadproc>(intialization_function))) {
+    LOG(FATAL) << "Cannot initialize OpenGL context with GLAD.";
+  }
+#ifndef NDEBUG
+  glEnable(GL_DEBUG_OUTPUT);
+  glDebugMessageCallback(MessageCallback, 0);
+#endif
+}
+
+}  // namespace gl
+
+#endif  // CODE_OPENGL_TUTORIALS_GL_CORE_INIT_H_
--- gl/core/main_test.cpp
+++ gl/core/main_test.cpp
@@ -1,10 +1,12 @@
 #include "gl/ui/glfw/viewer.h"
 
+#include "glog/logging.h"
 #include "gtest/gtest.h"
 
 using gl::glfw::Viewer;
 
 int main(int argc, char** argv) {
+  google::InitGoogleLogging(*argv);
   Viewer viewer{"TestViewer"};
   bool initialized = viewer.InitializeHidden();
   if (!initialized) { return 1; }
