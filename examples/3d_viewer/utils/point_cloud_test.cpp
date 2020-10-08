f2cd85d5ba18a1e52708e392de74e951c745102b
Igor Bogoslavskyi <igor.bogoslavskyi@gmail.com>
2020-10-08 21:15:35 +0200

    Add more tests

--- examples/3d_viewer/utils/point_cloud_test.cpp
+++ examples/3d_viewer/utils/point_cloud_test.cpp
@@ -12,10 +12,26 @@ TEST(PointCloudTest, InitEmpty) {
   EXPECT_EQ(0u, cloud.intensities().size());
 }
 
+TEST(PointCloudTest, InitPoints) {
+  PointCloud cloud{{{1, 2, 3}, {4, 5, 6}}};
+  EXPECT_EQ(2u, cloud.points().size());
+  EXPECT_EQ(0u, cloud.intensities().size());
+}
+
+TEST(PointCloudTest, InitPointsAndIntensities) {
+  PointCloud cloud{{{1, 2, 3}, {4, 5, 6}}, {42, 42}};
+  EXPECT_EQ(2u, cloud.points().size());
+  EXPECT_EQ(2u, cloud.intensities().size());
+}
+
 TEST(PointCloudTest, InitKitti) {
   auto cloud_ptr =
       PointCloud::FromFile("examples/3d_viewer/utils/test_data/cloud.txt");
   ASSERT_NE(nullptr, cloud_ptr);
   EXPECT_EQ(121796u, cloud_ptr->points().size());
   EXPECT_EQ(121796u, cloud_ptr->intensities().size());
+}
+
+TEST(PointCloudDeathTest, DifferentNumberOfPointsAndIntensities) {
+  EXPECT_DEATH(PointCloud({{1, 2, 3}, {4, 5, 6}}, {42}), "Check failed:.*");
 }
